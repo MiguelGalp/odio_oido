5da8c860394c8b0a46562671a64ff8afb353c2b7
Miguel Galperin <miguel.galperin@gmail.com>
2023-11-17 19:22:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+
 import os
 
 
