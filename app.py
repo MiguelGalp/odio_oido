7724d57150057b2be33ec8db353a8fa8e8e50db1
Miguel Galperin <miguel.galperin@gmail.com>
2023-11-21 16:51:52 -0300

    Engagement

--- app.py
+++ app.py
@@ -111,52 +111,54 @@ loop = asyncio.get_event_loop()
 loop.run_until_complete(fetch_tweets_and_update_engagement())
 
 
-def get_current_toxicity():
+def get_current_engagement():
     # Setup last fetch as the instance of db within this scope
-    total_increase_record = TotalIncrease.query.order_by(TotalIncrease.timestamp.desc()).first()
-    if total_increase_record is None:
-        app.logger.warning("No TotalIncrease records found")
-        return jsonify({"error": "No TotalIncrease records found"})
-    # Retrieve the total tweet increase of last fetch
-    total_tweet_increase = total_increase_record.total_tweet_increase
-    app.logger.info(f"Number of TotalIncrease records: {len(TotalIncrease.query.all())}")
+    last_tweet = Tweet.query.order_by(Tweet.id.desc()).first()
+    if last_tweet is None:
+        app.logger.warning("No Tweet records found")
+        return jsonify({"error": "No Tweet records found"})
+    # Retrieve the engagement metrics of last tweet
+    total_engagement = last_tweet.likes + last_tweet.retweets + last_tweet.replies
+    app.logger.info(f"Total engagement of last tweet: {total_engagement}")
     # The app´s principal logic is represented here
-    if total_tweet_increase < 3:
-        return jsonify({"toxicity": "Green"})
-    elif total_tweet_increase < 5:
-        return jsonify({"toxicity": "Yellow"})
+    if total_engagement < 100:
+        return jsonify({"engagement": "Green"})
+    elif total_engagement < 500:
+        return jsonify({"engagement": "Yellow"})
     else:
-        return jsonify({"toxicity": "Red"})
-@app.route('/toxicity', methods=['GET'])
-def toxicity_route():
-    return get_current_toxicity()
-from dateutil.parser import parse
+        return jsonify({"engagement": "Red"})
+
+@app.route('/engagement', methods=['GET'])
+def engagement_route():
+    return get_current_engagement()
+
 @app.route('/tweet_activity', methods=['GET'])
 def tweet_activity_route():
-    # Fetch the last four hours of TotalIncrease records
+    # Fetch the last four hours of Tweet records
     four_hours_ago = datetime.now(timezone.utc) - timedelta(hours=4)
-    records = TotalIncrease.query.filter(TotalIncrease.timestamp >= four_hours_ago).all()
+    records = Tweet.query.filter(Tweet.timestamp >= four_hours_ago).all()
     # Ensure all timestamps are timezone-aware
     for record in records:
         if record.timestamp.tzinfo is None or record.timestamp.tzinfo.utcoffset(record.timestamp) is None:
             record.timestamp = record.timestamp.replace(tzinfo=timezone.utc)
-    # Calculate the total tweet increase for each hour
-    increases = []
+    # Calculate the total engagement for each hour
+    engagements = []
     for i in range(4):
         start_time = datetime.now(timezone.utc) - timedelta(hours=i+1)
         end_time = datetime.now(timezone.utc) - timedelta(hours=i)
-        increase = sum(record.total_tweet_increase for record in records if start_time <= record.timestamp < end_time)
-        increases.append((start_time, increase))
+        engagement = sum((record.likes + record.retweets + record.replies) for record in records if start_time <= record.timestamp < end_time)
+        engagements.append((start_time, engagement))
     # Check for any spikes in tweet activity
     spike_times = []
     for i in range(1, 4):
-        if increases[i][1] > 2 * sum(increase[1] for increase in increases[:i]):
-            spike_times.append(increases[i][0])
+        if engagements[i][1] > 2 * sum(engagement[1] for engagement in engagements[:i]):
+            spike_times.append(engagements[i][0])
     if spike_times:
         message = f"Spikes at the following times: {', '.join(time.strftime('%H:%M') for time in spike_times)}"
     else:
         message = "No significant spike in tweet activity in the last four hours."
     return jsonify({"message": message})
+
 
 @app.route('/')
 def index():
