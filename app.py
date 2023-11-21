bf457479c74c9d4376cb7407f4b4749f5f99f85c
Miguel Galperin <miguel.galperin@gmail.com>
2023-11-21 16:42:24 -0300

    20Tweets

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ Bootstrap(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
+    total_engagement = db.Column(db.Integer, default=0)
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
